60f661a8791231def379b4c94864c7ee142dcf46
SilmyAhamed <silmypc@gmail.com>
2022-08-20 16:30:12 +0530

    first commit

---
+++ EqualImplementation/src/main/Application.java
@@ -0,0 +1,30 @@
+package main;
+
+public class Application {
+
+	public static void main(String[] args) {
+		
+		Employee employee01 = new Employee();
+		//Integer emp01Id = employee01.id = 100;
+		//employee01.code = "100";
+//		String emp01Code = employee01.code;
+//		String emp01Name = employee01.name;
+//		String emp01Nic = employee01.nic;
+//		String emp01Address = employee01.address;
+		
+		Employee employee02 = new Employee();
+		//Integer emp02Id = employee02.id = 101;
+		//employee02.code = "100";
+//		String emp02Code = employee02.code;
+//		String emp02Name = employee02.name;
+//		String emp02Nic = employee02.nic;
+//		String emp02Address = employee02.address;
+		
+		//System.out.println(employee01.code == employee02.code);
+		//System.out.println(employee01.code.equals(employee02.code));
+		
+//		System.out.println(emp01Code == emp02Code);
+//		System.out.println(emp01Code.equals(emp02Code));
+		
+	}
+}
---
+++ EqualImplementation/src/main/Employee.java
@@ -0,0 +1,16 @@
+package main;
+
+public class Employee {
+
+//	public Integer id = 100;
+//	public String code = "V500";
+//	public String name = "Silmy";
+//	public String nic = "962743269v";
+//	public String address = "Ampara";
+	
+	public Integer id;
+	public String code;
+	public String name;
+	public String nic;
+	public String address;
+}
