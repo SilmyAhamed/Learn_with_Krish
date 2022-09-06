8b53beb5a4b25f8dbd6463263b6a4fa900320ddd
SilmyAhamed <silmypc@gmail.com>
2022-09-06 15:15:25 +0530

    Equal Implementation

--- EqualImplementation/src/main/Application.java
+++ EqualImplementation/src/main/Application.java
@@ -3,28 +3,53 @@ package main;
 public class Application {
 
 	public static void main(String[] args) {
-		
+
 		Employee employee01 = new Employee();
-		//Integer emp01Id = employee01.id = 100;
-		//employee01.code = "100";
-//		String emp01Code = employee01.code;
-//		String emp01Name = employee01.name;
-//		String emp01Nic = employee01.nic;
-//		String emp01Address = employee01.address;
+		employee01.setId(100);
+		employee01.setCode("V100");
+		employee01.setNic("962743269v");
+		employee01.setAddress("Ampara");
+		System.out.println("Employee ID : " + employee01.getId());
+		System.out.println("Employee CODE :" + employee01.getCode());
+		System.out.println("Employee NIC : " + employee01.getNic());
+		System.out.println("Employee ADDRESS : " + employee01.getAddress());
+		
+		System.out.println("-------------------------------------------------");
 		
 		Employee employee02 = new Employee();
-		//Integer emp02Id = employee02.id = 101;
-		//employee02.code = "100";
-//		String emp02Code = employee02.code;
-//		String emp02Name = employee02.name;
-//		String emp02Nic = employee02.nic;
-//		String emp02Address = employee02.address;
+		employee02.setId(200);
+		employee02.setCode("V200");
+		employee02.setNic("952743269v");
+		employee02.setAddress("Galle");
+		System.out.println("Employee ID : " + employee02.getId());
+		System.out.println("Employee CODE :" + employee02.getCode());
+		System.out.println("Employee NIC : " + employee02.getNic());
+		System.out.println("Employee ADDRESS : " + employee02.getAddress());
 		
-		//System.out.println(employee01.code == employee02.code);
-		//System.out.println(employee01.code.equals(employee02.code));
+		System.out.println("-------------------------------------------------");
 		
-//		System.out.println(emp01Code == emp02Code);
-//		System.out.println(emp01Code.equals(emp02Code));
+		System.out.println(employee01.getCode() == employee02.getCode());
+		System.out.println(employee01.equals(employee02.getCode()));
 		
+		System.out.println("-------------------------------------------------");
+		System.out.println("\n\n\n");
+		
+		Employee employee03 = new Employee();
+		employee03.setId(100);
+		employee03.setCode("V100");
+		employee03.setNic("962743269v");
+		employee03.setAddress("Ampara");
+		System.out.println("Employee ID : " + employee03.getId());
+		System.out.println("Employee CODE :" + employee03.getCode());
+		System.out.println("Employee NIC : " + employee03.getNic());
+		System.out.println("Employee ADDRESS : " + employee03.getAddress());
+		
+		System.out.println("-------------------------------------------------");
+		
+		System.out.println(employee01.getCode() == employee03.getCode());
+		System.out.println(employee01.equals(employee03.getCode()));
+		
+		System.out.println("-------------------------------------------------");
+
 	}
 }
--- EqualImplementation/src/main/Employee.java
+++ EqualImplementation/src/main/Employee.java
@@ -2,15 +2,52 @@ package main;
 
 public class Employee {
 
-//	public Integer id = 100;
-//	public String code = "V500";
-//	public String name = "Silmy";
-//	public String nic = "962743269v";
-//	public String address = "Ampara";
-	
-	public Integer id;
-	public String code;
-	public String name;
-	public String nic;
-	public String address;
+	int id;
+	String code;
+	String nic;
+	String address;
+
+	public Employee(int id, String code, String nic, String address) {
+		super();
+		this.id = id;
+		this.code = code;
+		this.nic = nic;
+		this.address = address;
+	}
+
+	public Employee() {
+
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public String getNic() {
+		return nic;
+	}
+
+	public void setNic(String nic) {
+		this.nic = nic;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 }
